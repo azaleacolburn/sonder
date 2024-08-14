b548b16aa6ceca6a2e387fea795d369935a2b0d7
Azalea Colburn <azalea.colburn@autodesk.com>
2024-08-13 17:57:21 -0700

    remove target :(

--- target/debug/deps/sonder_engine-4c88283ab22ce48b.d
+++
@@ -1,5 +0,0 @@
-/Users/colbura/compsci/sonder/target/debug/deps/libsonder_engine-4c88283ab22ce48b.dylib: sonder_engine/src/lib.rs
-
-/Users/colbura/compsci/sonder/target/debug/deps/sonder_engine-4c88283ab22ce48b.d: sonder_engine/src/lib.rs
-
-sonder_engine/src/lib.rs:
--- target/debug/deps/tests-e1e15a0e6942a754.d
+++
@@ -1,5 +0,0 @@
-/Users/colbura/compsci/sonder/target/debug/deps/tests-e1e15a0e6942a754: tests/src/main.rs
-
-/Users/colbura/compsci/sonder/target/debug/deps/tests-e1e15a0e6942a754.d: tests/src/main.rs
-
-tests/src/main.rs:
