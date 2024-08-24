7a5f324321e5acff59683fac3781687f66e63592
Azalea Colburn <elocolburn@comcast.net>
2024-08-24 14:27:33 -0700

    started parsing imported c functions to rust

--- sonder_engine/src/lib.rs
+++ sonder_engine/src/lib.rs
@@ -1,27 +1,181 @@
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream, TokenTree};
 use regex::Regex;
+use std::num::ParseIntError;
 use std::{fs, str::FromStr};
+use syn::{parse_macro_input, LitStr};
 
-#[proc_macro_attribute]
-pub fn import_c(metadata: TokenStream, input: TokenStream) -> TokenStream {
-    let parsed_input: Vec<String> = metadata
+#[proc_macro]
+pub fn import_c(input: TokenStream) -> TokenStream {
+    let parsed_input: Vec<String> = input
         .to_string()
-        .split(",")
+        .split(", ")
         .map(|n| n.to_owned())
         .collect();
-    let import_path = parsed_input[0].clone();
-    let import_item = parsed_input[1].clone();
-    let extern_file_contents = fs::read_to_string(import_path).expect("File doesn't exist");
-    let pattern = Regex::new(format!(r"(?m)^\s*\b(int|char|void)\b\s+{import_item}\s").as_str())
-        .expect("Invalid regex");
-    let match_line = pattern
-        .find(extern_file_contents.as_str())
+    let raw_path = parsed_input[0].clone().parse().unwrap();
+    let raw_item = parsed_input[1].clone().parse().unwrap();
+    let path = parse_macro_input!(raw_path as LitStr).value();
+    let item = parse_macro_input!(raw_item as LitStr).value();
+
+    let fc = fs::read_to_string(path).unwrap();
+
+    // TODO: Figure out how to allow typedefs, maybe preprocess c file first?
+    let pattern = Regex::new(
+        format!(r"^ *(int|char|void)( \*|\*|\* | ){item} ?\(((int|char|void)( \*|\*|\* | )[a-zA-Z0-9_]+,? ?)*\)")
+            .as_str(),
+    )
+    .expect("Invalid regex");
+
+    let declaration = pattern
+        .find(fc.as_str())
         .expect("No matches found")
         .as_str();
-    println!("{}", match_line);
+    println!("{}", declaration);
 
-    // TODO: figure out how to agree linking at comp time
-    // let res = TokenStream::from_str("")
+    let output = format!("println!(\"this macro needs to output something for now\");");
+    output.parse().unwrap()
+}
+/// This is where the lexical analysis happens
+fn c_string_to_tokens(buff: impl ToString) -> Result<Vec<Token>, ParseIntError> {
+    let mut ret: Vec<Token> = vec![];
+    let chars = buff.to_string().chars().collect::<Vec<char>>();
+    let mut curr: String = String::from("");
+    let mut i: usize = 0;
+    while i < chars.len() {
+        match chars[i] {
+            ' ' => {}
+            'i' => {
+                if chars[i + 1] == 'n' && chars[i + 2] == 't' && chars[i + 3] == ' ' {
+                    // split.push(String::from("int"));
+                    ret.push(Token::Type(RhTypes::Int));
+                    i += 2; // I think there's a problem with incrementing the iterator
+                } else {
+                    for j in i..chars.len() {
+                        if !chars[j].is_alphabetic() && chars[j] != '_' {
+                            break;
+                        }
+                        curr.push(chars[j]);
+                    }
+                    ret.push(Token::Id(curr.clone()));
+                    i += curr.len() - 1;
+                    curr = String::from("");
+                }
+            }
+            'c' => {
+                if chars[i + 1] == 'h'
+                    && chars[i + 2] == 'a'
+                    && chars[i + 3] == 'r'
+                    && chars[i + 4] == ' '
+                {
+                    // split.push(String::from("char"));
+                    ret.push(Token::Type(RhTypes::Char));
+                    i += 3;
+                } else {
+                    for j in i..chars.len() {
+                        if !chars[j].is_alphabetic() && chars[j] != '_' {
+                            break;
+                        }
+                        curr.push(chars[j]);
+                    }
+                    ret.push(Token::Id(curr.clone()));
+                    i += curr.len() - 1;
+                    curr = String::from("");
+                }
+            }
+            's' => {
+                if chars[i + 1] == 't'
+                    && chars[i + 2] == 'r'
+                    && chars[i + 3] == 'u'
+                    && chars[i + 4] == 'c'
+                    && chars[i + 5] == 't'
+                {
+                    ret.push(Token::Struct);
+                    i += 5;
+                }
+            }
+            '-' => {
+                ret.push(Token::Dash);
+            }
 
-    input
+            '*' => {
+                // split.push(String::from("*"));
+                ret.push(Token::Star); // The lexer can probably determine whether this is a mul or deref
+            }
+            // obviously none of this can be included in ids
+            '(' => {
+                ret.push(Token::OParen);
+            }
+            ')' => {
+                ret.push(Token::CParen);
+            }
+            '[' => ret.push(Token::OSquare),
+            ']' => ret.push(Token::CSquare),
+
+            ',' => {
+                // split.push(String::from(","));
+                ret.push(Token::Comma);
+            }
+            'v' => {
+                if chars[i + 1] == 'o'
+                    && chars[i + 2] == 'i'
+                    && chars[i + 3] == 'd'
+                    && (chars[i + 4] == ' ' || chars[i + 4] == '*')
+                {
+                    ret.push(Token::Type(RhTypes::Void));
+                    i += 3;
+                } else {
+                    for j in i..chars.len() {
+                        if !chars[j].is_alphabetic() && chars[j] != '_' {
+                            break;
+                        }
+                        curr.push(chars[j]);
+                    }
+                    ret.push(Token::Id(curr.clone()));
+                    i += curr.len() - 1;
+                    curr = String::from("");
+                }
+            }
+            _ => {
+                // if we'e here it's an identifier
+                for j in i..chars.len() {
+                    if !chars[j].is_alphabetic() && chars[j] != '_' {
+                        break;
+                    }
+                    curr.push(chars[j]);
+                }
+                ret.push(Token::Id(curr.clone()));
+                i += curr.len() - 1;
+                curr = String::from("");
+            }
+        }
+        i += 1;
+    }
+    Ok(ret)
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Token {
+    Fn,
+    Type(RhTypes),
+    Struct,
+    Star,
+    Id(String),
+    OParen,
+    CParen,
+    OSquare,
+    CSquare,
+    Dot,
+    Comma,
+    Dash,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum RhTypes {
+    Char,
+    Int,
+    Void,
+}
+
+fn parsed_c_declaration_to_rust(tokens: Vec<Tokens>) -> TokenStream {
+    let 
+    let parsed = format!("fn {}");
 }
--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,6 +1,5 @@
 use sonder_engine::import_c;
 
 fn main() {
-    #[import_c(~/compsci/sonder/import_src/test.c, test_func)]
-    let test = 5;
+    import_c!("import_src/test.c", "test_func");
 }
