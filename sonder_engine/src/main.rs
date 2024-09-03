60b2bd3150b2b0252bd6c8929bc7c16af2ab36ba
Azalea Colburn <elocolburn@comcast.net>
2024-09-03 16:24:52 -0700

    initial parsing

--- sonder_engine/src/main.rs
+++ sonder_engine/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use lang_c::driver::parse;
+
+fn analyze_file() {
+    let config = lang_c::driver::Config::with_gcc();
+    let ast = parse(&config, "import_src/test.c").expect("Failed to parse C Code");
 }
+
+fn analyze(tree: ) {}
