a8149d3185271c528769a2f0d59def400fc2e602
Azalea Colburn <elocolburn@comcast.net>
2024-10-08 15:42:32 -0700

    feat: is_mut function for pointers

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,5 +1,5 @@
 use crate::error::ErrType as ET;
-use crate::parser::TokenNode as Node;
+use crate::parser::{AssignmentOpType, NodeType, TokenNode as Node};
 
 enum PtrType {
     ConstPtrConst,
@@ -28,4 +28,27 @@ struct Function<'a> {
 
 struct Arena {
     data: Vec<StackData>,
+}
+
+/// Checks if a pointer is ever defererenced and modified
+fn is_mut(ptr_name: &String, root: &Node) -> bool {
+    for child in root.children.as_ref().unwrap().iter().filter(|child| {
+        std::mem::discriminant(&child.token)
+            == std::mem::discriminant(&NodeType::Assignment(AssignmentOpType::Eq))
+    }) {
+        let deref = &child.children.as_ref().unwrap()[0];
+        let expression = &deref.children.as_ref().unwrap();
+        if deref.token == NodeType::DeRef && expression.len() == 1 {
+            if let NodeType::Id(ptr) = &expression[0].token {
+                if *ptr == *ptr_name {
+                    return true;
+                }
+            }
+        }
+        if is_mut(ptr_name, &child) {
+            return true;
+        }
+    }
+
+    return false;
 }
