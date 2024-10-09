26b27b15b91e0aa1e494394412466569a3322d2d
Azalea Colburn <elocolburn@comcast.net>
2024-10-09 15:44:43 -0700

    feat: start pointer counting function

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -2,6 +2,8 @@ use crate::error::ErrType as ET;
 use crate::parser::{AssignmentOpType, NodeType, TokenNode as Node};
 
 enum PtrType {
+    TrueRaw,
+
     ConstPtrConst,
     ConstPtrMut,
     MutPtrConst,
@@ -14,6 +16,7 @@ enum PtrType {
 struct Ptr {
     name: String,
     t: PtrType,
+    is_mut: bool,
 }
 
 struct StackData {
@@ -30,6 +33,23 @@ struct Arena {
     data: Vec<StackData>,
 }
 
+fn get_all_pointers(var_name: String, root: &Node) -> Vec<Ptr> {
+    root.children
+        .as_ref()
+        .unwrap()
+        .iter()
+        .filter(|child| child.token == NodeType::Adr(var_name.clone()))
+        .map(|child| {
+            let is_mut = is_mut();
+            let ptr = Ptr {
+                name: var_name.clone(),
+                t: PtrType::TrueRaw,
+                is_mut: false,
+            };
+        });
+    todo!()
+}
+
 /// Checks if a pointer is ever defererenced and modified
 fn is_mut(ptr_name: &String, root: &Node) -> bool {
     for child in root.children.as_ref().unwrap().iter().filter(|child| {
@@ -38,11 +58,12 @@ fn is_mut(ptr_name: &String, root: &Node) -> bool {
     }) {
         let deref = &child.children.as_ref().unwrap()[0];
         let expression = &deref.children.as_ref().unwrap();
-        if deref.token == NodeType::DeRef && expression.len() == 1 {
-            if let NodeType::Id(ptr) = &expression[0].token {
-                if *ptr == *ptr_name {
-                    return true;
-                }
+        if !(deref.token == NodeType::DeRef) || !(expression.len() == 1) {
+            continue;
+        }
+        if let NodeType::Id(ptr) = &expression[0].token {
+            if *ptr == *ptr_name {
+                return true;
             }
         }
         if is_mut(ptr_name, &child) {
