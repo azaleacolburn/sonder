9e982fc36668fe5010eb86eb7a367a60d57f2814
Azalea Colburn <azalea.colburn@autodesk.com>
2024-10-09 21:54:00 -0700

    feat: added mutability checking to get_pointers function

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -33,41 +33,66 @@ struct Arena {
     data: Vec<StackData>,
 }
 
-fn get_all_pointers(var_name: String, root: &Node) -> Vec<Ptr> {
+/// Returns a vector of all pointers pointing to this id
+/// Note: It only returns explicit pointers
+fn get_all_pointers(var_name: &String, root: &Node) -> Vec<Ptr> {
     root.children
         .as_ref()
         .unwrap()
         .iter()
-        .filter(|child| child.token == NodeType::Adr(var_name.clone()))
-        .map(|child| {
-            let is_mut = is_mut();
-            let ptr = Ptr {
-                name: var_name.clone(),
-                t: PtrType::TrueRaw,
-                is_mut: false,
-            };
-        });
-    todo!()
+        .flat_map(|child| {
+            // TODO: Figure out how to check what pointers point to what, since some have names and
+            // some don't
+            // There's a difference between dereferencing and referencing that needs to be resolved
+            let mut sub_ptrs = get_all_pointers(var_name, child);
+            if child.token == NodeType::Adr(var_name.clone()) {
+                // TODO: We actually want to traverse the tree upwards here
+                let is_mut = if std::mem::discriminant(&root.token)
+                    == std::mem::discriminant(&NodeType::Assignment(AssignmentOpType::Eq))
+                {
+                    match &root.children.as_ref().unwrap()[0].token {
+                        // TODO: Should look start with the parent node of root instead
+                        NodeType::Id(id) => is_mut(&id, root),
+                        _ => panic!("Expected first assignment child to be id"),
+                    }
+                } else {
+                    false
+                };
+                // let is_mut = is_mut();
+                let ptr = Ptr {
+                    name: var_name.clone(),
+                    t: PtrType::TrueRaw,
+                    is_mut,
+                };
+                sub_ptrs.push(ptr);
+            }
+            sub_ptrs
+        })
+        .collect()
 }
 
 /// Checks if a pointer is ever defererenced and modified
 fn is_mut(ptr_name: &String, root: &Node) -> bool {
-    for child in root.children.as_ref().unwrap().iter().filter(|child| {
-        std::mem::discriminant(&child.token)
-            == std::mem::discriminant(&NodeType::Assignment(AssignmentOpType::Eq))
-    }) {
+    for child in root.children.as_ref().unwrap().iter() {
+        if is_mut(ptr_name, &child) {
+            return true;
+        }
+        if std::mem::discriminant(&child.token)
+            != std::mem::discriminant(&NodeType::Assignment(AssignmentOpType::Eq))
+        {
+            continue;
+        }
+
         let deref = &child.children.as_ref().unwrap()[0];
         let expression = &deref.children.as_ref().unwrap();
         if !(deref.token == NodeType::DeRef) || !(expression.len() == 1) {
             continue;
         }
+
         if let NodeType::Id(ptr) = &expression[0].token {
             if *ptr == *ptr_name {
                 return true;
             }
-        }
-        if is_mut(ptr_name, &child) {
-            return true;
         }
     }
 
