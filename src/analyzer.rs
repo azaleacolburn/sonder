23174d7c7fcc7298fd2c65a7eaa6b98fd102ffa3
Azalea Colburn <azalea.colburn@autodesk.com>
2024-10-10 22:08:11 -0700

    feat: fix analyzer panics on unwrapping children vecs

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -20,16 +20,19 @@ pub struct Ptr {
     is_mut: bool,
 }
 
+#[derive(Debug)]
 struct StackData {
     occurences: Vec<Node>,
     refs: Vec<Ptr>, // type_t: we don't care about how large data is
 }
 
+#[derive(Debug)]
 struct Function<'a> {
     ptr_params: Vec<&'a StackData>,
     owned_params: Vec<&'a StackData>,
 }
 
+#[derive(Debug)]
 struct Arena {
     data: Vec<StackData>,
 }
@@ -44,22 +47,21 @@ pub enum AssignmentBool {
 /// Note: It only returns explicit pointers
 /// TODO: Make pointer grabbing and mut checking for every variable a single sweep over the tree
 pub fn get_all_pointers(var_name: &String, root: &Node, is_assignment: AssignmentBool) -> Vec<Ptr> {
-    root.children
-        .as_ref()
-        .unwrap()
-        .iter()
-        .flat_map(|child| {
-            let this_is_assignment = if let NodeType::Assignment(_) = &child.token {
-                let is_mut = match &child.children.as_ref().unwrap()[0].token {
-                    NodeType::Id(id) => is_mut(&id, root),
-                    _ => panic!("Expeceter first assignment child to be id"),
-                };
-                AssignmentBool::IsAssignment(is_mut)
-            } else {
-                is_assignment
+    println!("root: {:?}\n", root);
+    let mut ptrs: Vec<Ptr> = vec![];
+
+    let this_is_assignment = match &root.token {
+        NodeType::Assignment(_) => {
+            let deref = &root.children.as_ref().unwrap()[0];
+            let is_mut = match &deref.children.as_ref().unwrap()[0].token {
+                // FIXME: This should start searching in root's parent
+                NodeType::Id(id) => is_mut(&id, root),
+                _ => panic!("Expected first assignment child to be id"),
             };
-            let mut sub_ptrs = get_all_pointers(var_name, child, this_is_assignment);
-            if child.token == NodeType::Adr(var_name.clone()) {
+            AssignmentBool::IsAssignment(is_mut)
+        }
+        NodeType::Adr(adr_name) => {
+            if adr_name == var_name {
                 let is_mut = match &is_assignment {
                     AssignmentBool::IsAssignment(is_mut) => *is_mut,
                     AssignmentBool::NotAssignment => false,
@@ -69,16 +71,30 @@ pub fn get_all_pointers(var_name: &String, root: &Node, is_assignment: Assignmen
                     t: PtrType::TrueRaw,
                     is_mut,
                 };
-                sub_ptrs.push(ptr);
+                ptrs.push(ptr);
             }
-            sub_ptrs
-        })
-        .collect()
+
+            is_assignment
+        }
+        _ => is_assignment,
+    };
+
+    if let Some(children) = &root.children {
+        let mut sub_pointers = children
+            .iter()
+            .flat_map(|child| get_all_pointers(var_name, child, this_is_assignment))
+            .collect::<Vec<Ptr>>();
+        ptrs.append(&mut sub_pointers);
+    }
+
+    ptrs
 }
 
 /// Checks if a pointer is ever defererenced and modified
 pub fn is_mut(ptr_name: &String, root: &Node) -> bool {
-    for child in root.children.as_ref().unwrap().iter() {
+    // TODO: Fix this being scuffed
+    let blank = vec![];
+    for child in root.children.as_ref().unwrap_or_else(|| &blank).iter() {
         if is_mut(ptr_name, &child) {
             return true;
         }
